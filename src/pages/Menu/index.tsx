118097cd67e145783c4965bb442ee63d9c2999de
Diana <diana45637@live.com>
2022-07-02 23:06:20 -0300

    fix/style

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,11 @@
 import styles from './Menu.module.scss';
-import logo from '../../assets/logo.png'
+import logo from 'assets/logo.png'
 
 export default function Menu() {
     return (
         <main>
             <nav className={styles.menu}>
-                <img src={logo} alt='La Cucina Vegana' />
+                <img src={logo} alt='La Cucina Vegana'  width={380} />
             </nav>
         </main>
     )
