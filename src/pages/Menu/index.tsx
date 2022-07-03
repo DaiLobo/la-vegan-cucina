be62f8f3aa4f7531fceebea2950309c3154cb97e
Diana <diana45637@live.com>
2022-07-02 23:39:07 -0300

    feature/font

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,14 @@
 import styles from './Menu.module.scss';
-import logo from 'assets/logo.png'
+import logo from 'assets/logo.png';
+import 'normalize.css';
+
 
 export default function Menu() {
     return (
         <main>
             <nav className={styles.menu}>
                 <img src={logo} alt='La Cucina Vegana'  width={380} />
+                La cucina vegana
             </nav>
         </main>
     )
